eaa3f245dc89c6e231c1c420bc5a4450cbff4a04
Stino01 <agostino.carrieri01@gmail.com>
2022-04-01 20:30:44 +0200

    paridispari with control

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -3,31 +3,11 @@
 
 let coin = prompt('Scegli pari o dispari');
 console.log(coin);
-let num = parseInt(prompt('Scegli un numero tra 1 e 5 compresi'));
-console.log('num1: ', num);
-
-// CONTROLLO
-/* if (coin !== 'pari' || coin !== 'dispari') {
-    console.log('Non hai scelto tra pari o dispari');
-} else {
-    let num = parseInt(prompt('Scegli un numero tra 1 e 5 compresi'));
-    if (num < 1 || num > 5) {
-        console.log('Il numero non si trova tra 1 e 5 compresi')
-    } else {
-        console.log(num);
-    };
-}; */
 
 // FUNZIONE NUMERI RANDOM
 function randomNum() {
     return Math.floor(Math.random() * 5) + 1;
 };
-
-let random = randomNum();
-console.log('num random: ', random);
-
-let somma = num + random;
-console.log('somma: ', somma);
 
 // FUNZIONE PARI E DISPARI
 function paridispari(num) {
@@ -38,11 +18,28 @@ function paridispari(num) {
     };
 }
 
-let tipoSomma = paridispari(somma);
-console.log(tipoSomma)
-
-if (coin === tipoSomma) {
-    console.log('hai vinto')
+// CONTROLLO
+if (coin !== 'pari' && coin !== 'dispari') {
+    console.log('Non hai scelto tra pari o dispari');
 } else {
-    console.log('hai perso')
-}
+    let num = parseInt(prompt('Scegli un numero tra 1 e 5 compresi'));
+    if (num < 1 || num > 5) {
+        console.log('Il numero non si trova tra 1 e 5 compresi')
+    } else {
+        console.log('num: ', num);
+        let random = randomNum();
+        console.log('num random: ', random);
+
+        let somma = num + random;
+        console.log('somma: ', somma);
+
+        let tipoSomma = paridispari(somma);
+        console.log(tipoSomma)
+
+        if (coin === tipoSomma) {
+            console.log('hai vinto')
+        } else {
+            console.log('hai perso')
+        }
+    };
+};
